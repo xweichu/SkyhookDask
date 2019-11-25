09fc3f114411c945b893839d2b53db65ff8b5d4a
Aaron Chu <xweichu@hotmail.com>
2019-11-25 01:38:31 -0800

    added common

--- skyhook.py
+++ skyhook.py
@@ -1,4 +1,5 @@
 from skyhook_common import *
+import time
 
 class SkyhookDM:
     def __init__(self):
@@ -30,3 +31,26 @@ class SkyhookDM:
 
         dataset = Dataset(data['dataset_name'], data['size'], files)
         return dataset
+    
+    def runQuery(self, obj, querystr):
+        if 'File' in str(type(obj)):
+            obj_prefix = obj.dataset + '.' + obj.name
+            brs = querystr.split(',')
+            objnames = []
+            for br in brs:
+                objnames.append(obj_prefix + '.' + br)
+
+            def runOnDriver(objname):
+                time.sleep(3)
+                return 1
+            
+            futures = []
+
+            for objname in objnames:
+                futures.append(self.client.submit(runOnDriver,objname))
+            
+            return self.client.gather(futures)
+        return 0
+                
+
+
